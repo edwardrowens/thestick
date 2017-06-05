aabe966e836192f5ed1986ebecb344955f34dcc1
edwardrowens <eddiebeme@gmail.com>
2017-06-04 17:20:56 -0700

    took wow out and added hover effect on header

--- src/frontend/container/header-container.js
+++ src/frontend/container/header-container.js
@@ -58,7 +58,7 @@ class HeaderContainer extends React.Component {
                     <div className="mdl-layout-spacer mdl-layout--large-screen-only"></div>
                     <nav className="mdl-navigation mdl-layout--large-screen-only" >
                         <TabComponent name={'home'} onClick={this.onClick} isActive={this.state.homeActive} text={'Home'} path={'/'} />
-                        <TabComponent name={'wow'} onClick={this.onClick} isActive={this.state.wowActive} text={'WoW'} path={'/wow'} />
+                        {/*<TabComponent name={'wow'} onClick={this.onClick} isActive={this.state.wowActive} text={'WoW'} path={'/wow'} />*/}
                         <TabComponent name={'overwatch'} onClick={this.onClick} isActive={this.state.overwatchActive} text={'Overwatch'} path={'/overwatch'} />
                         <TabComponent name={'heroes'} onClick={this.onClick} isActive={this.state.heroesActive} text={'Heroes'} path={'/heroes'} />
                     </nav>
