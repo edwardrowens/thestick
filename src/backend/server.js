44e5e5ce60584be105e3e27a0641371c8bbac0f9
Eddie Owens <edward.owens@gamblitgaming.com>
2017-06-07 18:02:06 -0700

    twitch api should work now

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,17 +1,51 @@
 const express = require('express')
 const path = require('path')
 const favicon = require('serve-favicon')
+const Client = require('node-rest-client').Client
 
 const app = express()
+var client = new Client()
 
 const PORT = process.env.PORT || 8000
 
 app.use(express.static(path.join(__dirname, '..', '..', 'lib', 'build')))
 app.use(favicon(path.join(__dirname, '..', '..', 'lib', 'build', 'assets', 'favicon.png')))
 
-app.get('*', function(req, res) {
-	res.sendFile(path.resolve(__dirname, '..', '..', 'lib', 'build', 'index.html'));
-});
+app.get('/', function (req, res, next) {
+	res.sendFile(path.join(__dirname, '..', '..', 'lib', 'build', 'index.html'))
+})
+
+app.get('/twitch/streaming', function (req, res) {
+	let args = {
+		headers: {
+			"Content-type": "application/json",
+			"Client-ID": process.env.TWITCH_CLIENT_ID
+		}
+	}
+	client.get('https://api.twitch.tv/kraken/streams/thesticktv', args, function (body, response) {
+		if (response.statusCode == 200) {
+			if (!body) {
+				console.log("no body present for: " + res.url)
+				res.status(response.statusCode).send()
+			}
+			res.status(200).send(body)
+		} else {
+			res.status(response.statusCode).send()
+		}
+	})
+})
+
+app.get('/overwatch', function (req, res, next) {
+	res.sendFile(path.join(__dirname, '..', '..', 'lib', 'build', 'index.html'))
+})
+
+app.get('/heroes', function (req, res, next) {
+	res.sendFile(path.join(__dirname, '..', '..', 'lib', 'build', 'index.html'))
+})
+
+app.get('*', function (req, res, next) {
+	res.redirect('/')
+})
 
 console.log('Server started on port ' + PORT)
 app.listen(PORT)
--- src/frontend/service/twitch-api-resource.js
+++ src/frontend/service/twitch-api-resource.js
@@ -3,18 +3,11 @@ import { Client } from 'node-rest-client'
 
 export default class TwitchApiResource {
     constructor() {
-        this.clientId = process.env.TWITCH_CLIENT_ID
     }
 
     send(callback) {
         let client = new Client()
-        let args = {
-            headers: {
-                "Content-type": "application/json",
-                "Client-ID" : this.clientId
-            }
-        }
-        client.get('https://api.twitch.tv/kraken/streams/thesticktv', args, (body, res) => {
+        client.get('http://thestick.herokuapp.com/twitch/streaming', (body, res) => {
             if (res.statusCode == 200) {
                 if (!body) {
                     console.log("no body present for: " + res.url)
@@ -23,7 +16,6 @@ export default class TwitchApiResource {
                 }
                 callback(body)
             } else {
-                console.error('Could not retrieve data for player ' + this.battleTag)
                 callback(null)
             }
         })
